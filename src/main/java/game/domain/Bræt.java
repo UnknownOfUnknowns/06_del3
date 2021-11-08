6c1bcee8f5d3ec573cc8be3c590f46bb140122ce
Hurtigpilskade <albertmatthaihalkjaer@gmail.com>
2021-11-08 14:51:26 +0100

    Spil opstart

--- src/main/java/game/domain/Bræt.java
+++ src/main/java/game/domain/Bræt.java
@@ -2,6 +2,7 @@ package game.domain;
 
 import game.Util.DomainFeltFactory;
 import game.domain.felter.Felt;
+import game.domain.felter.StartFelt;
 
 import java.io.FileNotFoundException;
 
@@ -15,5 +16,8 @@ public class Bræt {
     public Felt getFelt (int Øjne){
         // -2 for at forskyde terningernes værdi ind til array indekset
         return felter[Øjne-2];
+    }
+    public Felt getStartfelt(){
+        return felter[0];
     }
 }
---
+++ src/main/java/game/domain/Spil.java
@@ -0,0 +1,23 @@
+package game.domain;
+
+import game.Util.SpilData;
+
+import java.io.FileNotFoundException;
+import java.util.Date;
+
+public class Spil {
+    private Spiller[] spillere;
+    private Spiller vinder;
+    private Spiller tur_spiller;
+    private Bræt spillebræt;
+
+    public Spil (int antal_spillere) throws FileNotFoundException {
+        spillebræt = new Bræt();
+        spillere = new Spiller[antal_spillere];
+        for (Spiller spiller: spillere){
+            spiller = new Spiller(spillebræt.getStartfelt());
+        }
+        vinder = null;
+        tur_spiller = spillere[0];
+    }
+}
