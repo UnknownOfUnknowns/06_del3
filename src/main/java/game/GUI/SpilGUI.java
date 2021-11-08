11fb8cf81c417a8af079b34031edae1867ddd43b
Unknown Unknown <hans@bilstrupandersen.dk>
2021-11-08 14:32:04 +0100

    Opdatering af lokation af config

--- src/main/java/game/GUI/SpilGUI.java
+++ src/main/java/game/GUI/SpilGUI.java
@@ -1,4 +1,13 @@
 package game.GUI;
 
+import game.Util.DomainFeltFactory;
+import game.domain.Bræt;
+
+import java.io.FileNotFoundException;
+
 public class SpilGUI {
+    public static void main(String[] args) throws FileNotFoundException {
+        Bræt b = new Bræt();
+        System.out.println("SSS");
+    }
 }
