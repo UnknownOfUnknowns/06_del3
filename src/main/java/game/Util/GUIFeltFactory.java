f44a6e9a21e7102696e69a03bb8d3088de6bf93c
Unknown Unknown <hans@bilstrupandersen.dk>
2021-11-20 16:34:59 +0100

    Gui felter opdateret

--- src/main/java/game/Util/GUIFeltFactory.java
+++ src/main/java/game/Util/GUIFeltFactory.java
@@ -3,7 +3,9 @@ package game.Util;
 import game.domain.felter.EjendomsFelt;
 import game.domain.felter.HelleFelt;
 import game.domain.felter.StartFelt;
+import gui_fields.GUI_Empty;
 import gui_fields.GUI_Field;
+import gui_fields.GUI_Start;
 import gui_fields.GUI_Street;
 
 import java.awt.*;
@@ -23,11 +25,10 @@ public class GUIFeltFactory extends FeltFactory{
             String buffer = input.next();
             switch (buffer){
                 case "Helle" -> felter.add(new GUI_Street("Helle", "", "", "", Color.BLUE, Color.black));
-                case "Start" -> felter.add(new GUI_Street("Start", "", "", "", Color.RED, Color.black));
+                case "Start" -> felter.add(new GUI_Start("Start", "", "", Color.RED, Color.black));
                 default -> {
                     String pris = Integer.toString(input.nextInt()) + "$";
                     felter.add(new GUI_Street(buffer, pris, "", pris, Color.WHITE, Color.BLACK));
-                    break;
                 }
             }
         }
